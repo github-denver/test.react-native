e613cadae9bb6ca2b5dbfe1dec24ab9781f24ad4
denver.html <denver.html@daum.net>
2021-03-28 15:55:02 +0900

    파일 이동

---
+++ RNPressableCLI/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+
+const Button = props => {
+  return (
+    <Pressable
+      style={{
+        padding: 10,
+        backgroundColor: '#1abc9c',
+      }}
+      onPressIn={() => console.log('Press In')}
+      onPressOut={() => console.log('Press Out')}
+      onPress={() => console.log('Press')}
+      onLongPress={() => console.log('Long Press')}
+      delayLongPress={3000}
+      pressRetentionOffset={{
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50,
+        backgroundColor: '#ff0000',
+      }}
+      hitSlop={50}>
+      <Text style={{padding: 10, fontSize: 30}}>{props.title}</Text>
+    </Pressable>
+  );
+};
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+      }}>
+      <Button title="Pressable" />
+    </View>
+  );
+};
+
+export default App;
